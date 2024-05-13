4b3451484bd4f3c21355d66f32001532ed3e6a3b
EMON-BHAI <sharifvau4@gmail.com>
2024-05-14 00:42:40 +0800

    Update work.js

--- scripts/commands/work.js
+++ scripts/commands/work.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "work",
 	version: "1.0.1",
 	permission: 0,
-	credits: "ryuko",
+	credits: "Emon",
 	prefix: true,
 	description: "earning money",
 	category: "economy",
