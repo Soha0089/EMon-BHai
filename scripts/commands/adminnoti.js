2673052abf7b8bf6af0fc684b1ca64a81db47ddf
EMON-BHAI <sharifvau4@gmail.com>
2024-05-13 22:53:39 +0800

    Update adminnoti.js

--- scripts/commands/adminnoti.js
+++ scripts/commands/adminnoti.js
@@ -5,7 +5,7 @@ module.exports.config = {
     name: "adminnoti",
     version: "1.0.0",
     permission: 2,
-    credits: "ryuko",
+    credits: "Emon",
     description: "",
     prefix: true,
     category: "admin",
@@ -42,7 +42,7 @@ const getAtm = (atm, body) => new Promise(async (resolve) => {
 module.exports.handleReply = async function ({ api, event, handleReply, Users, Threads, getText }) {
     
     const moment = require("moment-timezone");
-      var gio = moment.tz("Asia/Manila").format("DD/MM/YYYY - HH:mm:s");
+      var gio = moment.tz("Asia/Dhaka").format("DD/MM/YYYY - HH:mm:s");
     const { threadID, messageID, senderID, body } = event;
     let name = await Users.getNameUser(senderID);
     switch (handleReply.type) {
